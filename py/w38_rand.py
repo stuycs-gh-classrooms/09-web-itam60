2e8c7060bbf03e872b854244a23b633c2301d84f
itam60 <itam60>
2024-05-29 14:34:46 -0400

    testing

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -2,8 +2,6 @@
 print("Content-type: text/html\n")
 
 from random import random
-def foo():
-    body = "style="background-color:pink;""
-
+print("""<body style="background-color:pink;">""")
 r=random()
 print("Lucky number: ",r)
