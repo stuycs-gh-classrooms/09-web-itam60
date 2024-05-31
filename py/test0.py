1cd2abe86074917cd69e3994cc3aa8d563a98235
itam60 <itam60>
2024-05-31 14:50:08 -0400

    ..

--- py/test0.py
+++ py/test0.py
@@ -12,29 +12,37 @@ HTML_HEADER = """
 
 <head>
 <meta charset="utf-8">
-<title>"""
-
+<title>Quiz time!</title>
+</head>
+<body>
+"""
 HTML_FOOTER = """
 </body>
 </html>
 """
-
+terms='a,b,c,d'
+definitions='1,2,3,4'
 
 data = cgi.FieldStorage()
-guess="form?!"
-if ('guess' in data):
-    guess = data['guess'].value
-faveanimal="fish"
-if ('faveanimal' in data):
-    faveanimal = data['faveanimal'].value
-
-html= HTML_HEADER
-html+= guess
-html+= """
-</title>
-</head>
-"""
-html+= '<h1>Hello ' + faveanimal + '</h1>'
+#guess="form?!"
+#if ('guess' in data):
+#    guess = data['guess'].value
+#faveanimal="fish"
+#if ('faveanimal' in data):
+#    faveanimal = data['faveanimal'].value
+html=HTML_HEADER
+#html+= guess
+#html+= """
+#</title>
+#</head>
+#"""
+html+="<h1>Quiz below</h1>"+"\n"
+termslist=terms.split(',')
+deflist=definitions.split(',')
+i=0
+while i<len(termslist):
+    html+=termslist[i]+': '+deflist[i]+'\n'
+    i+=1
 html+= '<br><a href="test0.html">Try Again</a>'
 html+= HTML_FOOTER
 print(html)
