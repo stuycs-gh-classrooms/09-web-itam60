d63820d961a8b2b0673f4bc6ef4d9afbd46fc720
itam60 <itam60>
2024-06-03 14:52:36 -0400

    .

--- py/test0.py
+++ py/test0.py
@@ -39,6 +39,11 @@ html=HTML_HEADER
 html+="<h1>Quiz below</h1>"+"\n"
 termslist=terms.split(',')
 deflist=definitions.split(',')
+e=0
+answerdict={}
+while e<len(termslist):
+    answerdict[termslist[e]]=deflist[e]
+    e+=1
 i0=0
 i1=0
 scrambledict={}
@@ -46,11 +51,25 @@ while len(termslist)>0:
     i0=random.randint(0,len(termslist)-1)
     i1=random.randint(0,len(deflist)-1)
     scrambledict[termslist.pop(i0)]=deflist.pop(i1)
+html+="""
+<form action='test0.py' method='GET'>
+"""
 for x in scrambledict:
-    html+="<input class='styled' type='button' value='"+x+"'>"
-    html+='<br>'
-    html+="<input class='styled' type='button' value='"+scrambledict[x]+"'>"
-    html+='<br>'
+    html+="<input class='styled' type='button' name='click0' value='"+x+"'>"
+    html+="<input class='styled' type='button' name='click1' value='"+scrambledict[x]+"'>"
+html+="</form>"
+
+data1=cgi.FieldStorage()
+click0='1'
+if ('click0' in data1):
+    click0 = data['click0'].value
+click1='methyl'
+if ('click1' in data):
+    click1 = data['click1'].value
+correct=0
+if answerdict[click0]==click1 or answerdict[click1]==click0:
+    correct+=1
+html+=str(correct)
 html+= '<br><a href="test0.html">Try Again</a>'
 html+= HTML_FOOTER
 print(html)
